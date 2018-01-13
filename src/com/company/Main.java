2d36a485b7e5f3469b9d309ea943a8d897f69154
szililaszlo <laszlo0919@gmail.com>
2018-01-13 17:28:36 +0100

    inital

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//1
+
+        //Crime típusú arrayList a teszteléshez
+        ArrayList<Crime> crimes = new ArrayList<>();
+
+        //Crími példány létrehozása és setterek segítségével értékadás
+        Crime crime = new Crime();
+        crime.setAddress("Pécs");
+        crime.setcDateTime("2017");
+        crime.setDistrict(1500);
+        crime.setBeat("aaa");
+        crime.setGrid(4214);
+        crime.setCrimeDescr("asdsa");
+        crime.setUcrNcicCode(222222);
+        crime.setLatitude(222.2F);
+        crime.setLongtitude(22224.2F);
+
+        //hozzáadás az arryaListhez
+        crimes.add(crime);
+
+        //metódus hívás
+        SacramentoUtil.printCSVFromSacramentoList(crimes,"C:\\Users\\László\\Downloads\\csv");
     }
 }
